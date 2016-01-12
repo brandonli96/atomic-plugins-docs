2f9d26c683cf08fce118d51a2113bb7e0b880e44
Imanol Martin <imanolm@ludei.com>
2016-01-12 19:01:47 +0100

    Missing files

---
+++ src/js/atomic.js
@@ -0,0 +1,42 @@
+/**
+ * @fileOverview
+ <h2>About Atomic Plugins</h2>
+ <p>Atomic Plugins provide an elegant and minimalist API and are designed with portability in mind from the beginning. Framework dependencies are avoided by design so the plugins can run on any platform and can be integrated with any app framework or game engine.
+ <br/><p>You can contribute and help to create more awesome plugins. </p>
+ <h3>Atomic Plugins repositories</h3>
+ <p>This is a list of all the Atomic Plugins available.</p> 
+ <ul>
+ <li><a href="https://github.com/CocoonIO/cocoon-canvasplus">Canvas+ legacies</a></li>
+ <li><a href="https://github.com/ludei/atomic-plugins-ads">Atomic Plugins for Ads</a></li>
+ <li><a href="https://github.com/ludei/atomic-plugins-inapps">Atomic Plugins for In-App Purchases</a></li>
+ <li><a href="https://github.com/ludei/atomic-plugins-social">Atomic Plugins for Social integration</a></li>
+ <li><a href="https://github.com/ludei/atomic-plugins-share">Atomic Plugins Share</a></li>
+ <li><a href="https://github.com/ludei/atomic-plugins-facebook/">Atomic Plugins Facebook</a></li>
+ <li><a href="https://github.com/ludei/atomic-plugins-gamecenter/">Atomic Plugins Game Center</a></li>
+ <li><a href="https://github.com/ludei/atomic-plugins-googleplaygames/">Atomic Plugins Google Play Games</a></li>
+ <li><a href="https://github.com/ludei/atomic-plugins-notifications">Atomic Plugins Notifications</a></li>
+ </ul>
+ <h3>Documentation</h3>
+ <p>Select the specific namespace below to open the relevant documentation section:</p>
+ <ul>
+ <li><a href="Cocoon.html">Cocoon</a></li>
+ <li><a href="http://ludei.github.io/atomic-plugins-docs/dist/doc/js/index.html">Canvas+ legacies</a></li>
+ <li><a href="http://ludei.github.io/atomic-plugins-docs/dist/doc/js/Cocoon.Ad.html">Ads</a></li>
+ <li><a href="http://ludei.github.io/atomic-plugins-docs/dist/doc/js/Cocoon.Ad.AdMob.html">AdMob</a></li>
+ <li><a href="http://ludei.github.io/atomic-plugins-docs/dist/doc/js/Cocoon.Ad.MoPub.html">MoPub</a></li>
+ <li><a href="http://ludei.github.io/atomic-plugins-docs/dist/doc/js/Cocoon.InApp.html">InApps</a></li>
+ <li><a href="http://ludei.github.io/atomic-plugins-docs/dist/doc/js/Cocoon.Social.html">Social</a></li>
+ <li><a href="http://ludei.github.io/atomic-plugins-docs/dist/doc/js/Cocoon.Share.html">Share</a></li>
+ <li><a href="http://ludei.github.io/atomic-plugins-docs/dist/doc/js/Cocoon.Social.Facebook.html">Facebook</a></li>
+ <li><a href="http://ludei.github.io/atomic-plugins-docs/dist/doc/js/Cocoon.Social.GameCenter.html">Game Center</a></li>
+ <li><a href="http://ludei.github.io/atomic-plugins-docs/dist/doc/js/Cocoon.Social.GooglePlayGames.html">Google Play Games</a></li>
+ <li><a href="http://ludei.github.io/atomic-plugins-docs/dist/doc/js/Cocoon.Notification.html">Notifications</a></li>
+ <li><a href="http://ludei.github.io/atomic-plugins-docs/dist/doc/js/Cocoon.Notification.Local.html">Local Notifications</a></li>
+ <li><a href="http://ludei.github.io/atomic-plugins-docs/dist/doc/js/Cocoon.Notification.Parse.html">Parse Notifications</a></li>
+ </ul>
+ <h3>Tools</h3>
+ <a href="http://forums.cocoon.io"><img src="img/cocoon-tools-1.png" /></a>
+ <a href="https://docs.cocoon.io"><img src="img/cocoon-tools-2.png" /></a>
+ <a href="http://cocoon.io/"><img src="img/cocoon-tools-3.png" /></a>
+ * @version 1.0
+ */
